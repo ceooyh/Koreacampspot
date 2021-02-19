0d5465addc98cf1ac1a1a46559a1cdffe1de3a57
phantom5820 <phantom5820@naver.com>
2021-02-19 16:36:33 +0900

    로그인 로그아웃 컨트롤러,

--- src/main/java/kcs/service/MemberService.java
+++ src/main/java/kcs/service/MemberService.java
@@ -1,7 +1,10 @@
 package kcs.service;
 
+import java.util.HashMap;
+
 import org.springframework.stereotype.Service;
 
+import kcs.dto.MemberDTO;
 import kcs.mapper.MemberMapper;
 
 @Service
@@ -12,6 +15,14 @@ public class MemberService {
 		super();
 		this.mapper = mapper;
 	}
-	
+
 	// 여기부터 서비스 메서드 작성
+	// 로그인 메서드 -성진
+	public MemberDTO login(String id, String pass) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("id", id);
+		map.put("pass", pass);
+		return mapper.login(map);
+	}
+	
 }
