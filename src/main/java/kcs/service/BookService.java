80000adccaf40f1e6a0dfa1a81f6db8f80457301
cgd0318 <cgd0318@naver.com>
2021-02-23 10:36:26 +0900

    캠핑장 예약내역(일반사용자) 구현(테스트x) -희원

--- src/main/java/kcs/service/BookService.java
+++ src/main/java/kcs/service/BookService.java
@@ -1,7 +1,10 @@
 package kcs.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import kcs.dto.BookDTO;
 import kcs.mapper.BookMapper;
 
 @Service
@@ -12,6 +15,12 @@ public class BookService {
 		super();
 		this.mapper = mapper;
 	}
-	
+
 	// 여기부터 서비스 메서드 작성
+
+	// 캠핑장 예약 내역 확인 (일반 사용자 - 마이페이지) - 희원,20210223
+	public List<BookDTO> getGuestBookList(String id) {
+		return mapper.getGuestBookList(id);
+	}
+	
 }
