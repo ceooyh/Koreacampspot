4700ef3a0f3c0d5fbbb2b5e58d8b46b21613b222
cronoss <ceojsqpqp@naver.com>
2021-02-24 11:23:37 +0900

    캠핑장서치 기본틀 작성-505

--- src/main/java/kcs/controller/SpotController.java
+++ src/main/java/kcs/controller/SpotController.java
@@ -1,6 +1,7 @@
 package kcs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kcs.service.SpotService;
 
@@ -12,6 +13,11 @@ public class SpotController {
 		super();
 		this.service = service;
 	}
-
+	
 	// 여기부터 RequestMapping 처리
+	
+	@RequestMapping("/findCampSpotView.do")
+    public String findCampSpotView() {
+        return "spot/spot_search";
+    }
 }
