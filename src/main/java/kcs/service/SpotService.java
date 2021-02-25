f3fed915775486cc36556d2d124179993f82f5ef
cgd0318 <cgd0318@naver.com>
2021-02-25 15:13:56 +0900

    캠핑장 찾기 기본 목록 띄우기 구현중 -희원

--- src/main/java/kcs/service/SpotService.java
+++ src/main/java/kcs/service/SpotService.java
@@ -18,14 +18,14 @@ public class SpotService {
 
 	// 여기부터 서비스 메서드 작성
 
-	// 캠핑장 찾기 초기 목록 - 희원,20210224
-	public List<SpotDTO> getCampList() {
-		return mapper.getCampList();
+	// 캠핑장 별점 평균 - 희원,20210225
+	public double getStarAvg(int contentId) {
+		return mapper.getStarAvg(contentId);
 	}
 
-	// 캠핑장 검색 (캠핑장 이름, 키워드-태그, 지역) - 희원,20210224
-	public List<SpotDTO> getCampSearchList(String search) {
-		return mapper.getCampSearchList(search);
+	// 캠핑장 리뷰 개수 - 희원,20210225
+	public int getReviewCount(int contentId) {
+		return mapper.getReviewCount(contentId);
 	}
 	
 }
